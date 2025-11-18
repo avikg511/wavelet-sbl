c9ce4fb68ad6cb062d3fa5bfbf22db30915751b5
Avik Ghosh <a.ghosh24@outlook.com>
2025-11-18 11:43:35 -0800

    fix: convert module to just a function that can be called from REPL to avoid recompiling script on every single run. time to first plot decreased heavily

--- main.jl
+++ main.jl
@@ -1,5 +1,4 @@
-#=
-    @file main.jl
+#=  @file main.jl
     @author Avik Ghosh
     @date November 16th, 2025
 
@@ -9,29 +8,49 @@
                 (3) Sparse Bayesian Learning
                 (4) SBL using Symlets
 
-            More detail about each implementation can be found in the implementation specific files
+    More detail about each implementation can be found in the implementation specific files
 =#
-module MainModule
 
 # Includes and Using Statements
-include("./src/beamformer.jl")
+include("./src/bf_controller.jl")
 
-using .Beamformer
+using .BFController
+using .SwellConfigs
 
-# 1. Conventional Beamforming
-method = Beamformer.conventional
-process(method)
+function run()
+    # Data is not provided here, but is in a .mat file.
+    filePath = joinpath(pwd(), "snapshotData64.mat")
 
-# # 2. Conventional Beamforming with Symlets
-# method = Beamformer.conventional_symlets
-# process(method)
-# 
-# # 3. Sparse Bayesian Learning 
-# method = Beamformer.sbl 
-# process(method)
-# 
-# # 4. Sparse Bayesian Learning with Symlets
-# method = Beamformer.sbl_symlets
-# process(method)
+    # Create Config Struct with defaults. Everything else other than the experiment config is default
+    # and constant. The experiment config will be updated based on the data file itself.
+    T::Type = UInt32
+    U::Type = Float64
+    V::Type = ComplexF64
 
-end # module
+    convergence = ConvergenceConfig{T, U}()
+    sensor = SensorConfig{U}()
+    geometry = GeometryConfig{T, U}()
+    sbl = SBLConfig{T}()
+    wavelet = WaveletConfig{T}()
+
+    # Data is all in the form of Complex Float64 points, so set experiment config accordingly
+    experiment = ExperimentConfig{T, U, V}()
+
+    cfgs = PhysicalConfigs{T, U, V}(convergence, sensor, sbl, wavelet, geometry, experiment)
+
+    # 1. Conventional Beamforming
+    method = BFController.conventional
+    process(method, cfgs, filePath)
+
+    # # 2. Conventional Beamforming with Symlets
+    # method = BFController.conventional_symlets
+    # process(method)
+    
+    # 3. Sparse Bayesian Learning 
+    method = BFController.sbl 
+    process(method, cfgs, filePath)
+    
+    # # 4. Sparse Bayesian Learning with Symlets
+    # method = BFController.sbl_symlets
+    # process(method)
+end
